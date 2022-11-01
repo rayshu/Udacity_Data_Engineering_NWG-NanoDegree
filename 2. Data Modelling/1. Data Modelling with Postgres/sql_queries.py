1a5599cf25e21480a019fa62edd45baa170ed867
rayshu <er.rshmi@gmail.com>
2022-11-02 00:00:26 +0530

    Updates after reviews

--- 2. Data Modelling/1. Data Modelling with Postgres/sql_queries.py
+++ 2. Data Modelling/1. Data Modelling with Postgres/sql_queries.py
@@ -9,7 +9,7 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 # CREATE TABLES
 
 songplay_table_create = (""" CREATE TABLE IF NOT EXISTS songplays (
-songplay_id INT PRIMARY KEY,
+songplay_id SERIAL PRIMARY KEY,
 start_time TIMESTAMP NOT NULL,
 user_id INT NOT NULL,
 level TEXT,
@@ -61,14 +61,14 @@ weekday INT
 
 # INSERT RECORDS
 
-songplay_table_insert = (""" INSERT INTO songplays(songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
-VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s) 
+songplay_table_insert = (""" INSERT INTO songplays(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
+VALUES(%s, %s, %s, %s, %s, %s, %s, %s) 
 ON CONFLICT(songplay_id) DO NOTHING;
 """)
 
 user_table_insert = (""" INSERT INTO users(user_id, first_name, last_name, gender, level)
 VALUES(%s, %s, %s, %s, %s)
-ON CONFLICT(user_id) DO NOTHING;
+ON CONFLICT(user_id) DO UPDATE SET level = excluded.level;
 """)
 
 song_table_insert = (""" INSERT INTO songs (song_id, title, artist_id, year, duration)
